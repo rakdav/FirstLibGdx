8752d7b91f5a99c267c7dfd78abbbc7b94844159
Andrey <rakdav@inbox.ru>
2023-04-02 15:49:33 +0200

    part 1

--- core/src/com/mygdx/game/MainGameScreen.java
+++ core/src/com/mygdx/game/MainGameScreen.java
@@ -1,26 +1,54 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.mygdx.game.actors.ActorJugador;
+import com.mygdx.game.actors.ActorPinchos;
 
 public class MainGameScreen extends BaseScreen {
 
+	private Stage stage;
+	private ActorJugador jugador;
+	private ActorPinchos pinchos;
+	private Texture textureJugator,texturaPinchos;
+	private TextureRegion regionPinchos;
 	public MainGameScreen(MainGame game) {
-
+		super(game);
+		texturaPinchos=new Texture("pinchos.jpg");
+		textureJugator=new Texture("minijoe.jpg");
+		regionPinchos=new TextureRegion(texturaPinchos,0,64,128,64);
 	}
-
 	@Override
 	public void show() {
-
+		stage=new Stage();
+		jugador=new ActorJugador(textureJugator);
+		pinchos=new ActorPinchos(regionPinchos);
+		stage.addActor(jugador);
+		stage.addActor(pinchos);
+		jugador.setPosition(20,100);
+		pinchos.setPosition(500,100);
 	}
 
 	@Override
 	public void render(float delta) {
-
+		Gdx.gl.glClearColor(0.4f,0.5f,0.8f,1f);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		stage.act();
+		stage.draw();
 	}
 
+	private void compobarColisiones()
+	{
+		if(jugador.isAlive()&&jugador.getX()+jugador.getWidth()> pinchos.getX())
+		{
+			System.out.println("Collision");
+			jugador.setAlive(false);
+		}
+	}
 	@Override
 	public void resize(int width, int height) {
 
@@ -38,11 +66,11 @@ public class MainGameScreen extends BaseScreen {
 
 	@Override
 	public void hide() {
-
+		stage.dispose();
 	}
 
 	@Override
 	public void dispose() {
-
+		textureJugator.dispose();
 	}
 }
--- core/src/com/mygdx/game/actors/ActorPinchos.java
+++ core/src/com/mygdx/game/actors/ActorPinchos.java
@@ -1,4 +1,26 @@
 package com.mygdx.game.actors;
 
-public class ActorPinchos {
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+public class ActorPinchos extends Actor
+{
+    private TextureRegion pinchos;
+
+    public ActorPinchos(TextureRegion pinchos) {
+        this.pinchos = pinchos;
+        setSize(pinchos.getRegionWidth(),pinchos.getRegionHeight());
+    }
+
+    @Override
+    public void act(float delta) {
+        setX(getX()-250*delta);
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        batch.draw(pinchos,getX(),getY());
+    }
 }
